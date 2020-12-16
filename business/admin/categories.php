f7293ddc3863376e77699bb62f77869ef7b3aaf3
sfsobeng <sfsobeng@gmail.com>
2020-12-16 22:29:43 +0000

    all about update
    update

--- business/admin/categories.php
+++ business/admin/categories.php
@@ -15,9 +15,59 @@
       <div class="row">
         <div class="col-sm-4">
           <div class="form-group">
-            <label>Add Category</label>
-            <input type="text" name="category" placeholder="Add Category" class="form-control">
-            <input type="submit" name="submit" value="Add" class="btn btn-primary mt-2">
+              <label>Add Category</label>
+
+              <!-- Query to insert categories -->
+              <?php 
+              
+              if(isset($_POST['submit'])){
+
+                $cat_title = $_POST['cat_title'];
+
+                // validate empty fields
+                if($cat_title == "" || empty($cat_title)){
+                  echo "This field cannot be empty";
+                }else{
+                  // insert category to db
+                  // $query = "INSERT INTO categories(cat_title) VALUES('$cat_title')";     //do this or the below...both are same
+
+                  $query = "INSERT INTO categories(cat_title)";
+                  $query .= "VALUES ('$cat_title')";
+                  
+                  $create_category_query = mysqli_query($connection, $query);
+                  echo "New Category Inserted";
+                }
+              }
+              ?>
+
+              <form action="" method="post">
+                  <input type="text" name="cat_title" placeholder="Add Category" class="form-control">
+                  <input type="submit" name="submit" value="Add Category" class="btn btn-primary mt-2">
+              </form><br><br>
+
+              <!-- Edit form -->
+              <form action="" method="post">
+                <!-- Get edit value -->
+                <?php 
+                  if (isset($_GET['edit'])) {
+                    $cat_id = $_GET['edit'];
+                    $query = "SELECT * FROM CATEGORIES WHERE cat_id = $cat_id";
+                    $select_all_categories_id = mysqli_query($connection, $query);
+
+                    while ($row = mysqli_fetch_assoc($select_all_categories_id)) {
+                      $cat_id = $row['cat_id'];
+                      $cat_title = $row['cat_title'];
+                  ?>
+
+                      
+                      <input value="<?php if(isset($cat_title)){echo $cat_title; } ?>" type="text" name="cat_title" placeholder="Edit Category" class="form-control">
+
+
+                   <?php } } ?>
+
+                  <input type="submit" name="edit" value="Edit Category" class="btn btn-primary mt-2">
+              </form>
+
           </div>
         </div>
         <div class="col-sm-8">
@@ -44,23 +94,32 @@
                     $cat_title = $row['cat_title'];
 
                     
-                  echo <tr>
-                  echo <td>Jonas Alexander</td>
-                  echo <td>Developer</td>
-                  echo <td>Aviation</td>
-                  </tr>
+                  echo "<tr>";
+                  echo "<td>{$cat_title}</td>";
+                  echo "<td><a href='categories.php?edit={$cat_id}' class='btn btn-warning'>Edit</a></td>";
+                  echo "<td><a href='categories.php?delete={$cat_id}' class='btn btn-danger'>Delete</a></td>";
+                  echo "</tr>";
 
-                  }
-                  
+                  }  
                   ?>
 
+                    <!-- delete category -->
+                    <?php 
+
+                      if(isset($_GET['delete'])){
+
+                        $the_cat_id = $_GET['delete'];
+                        $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
+                        $delete_query = mysqli_query($connection, $query);
+
+                        header("Location: categories.php");
+                      }
+                    
+                    ?>
 
 
-                <tr>
-                  <td>Jonas Alexander</td>
-                  <td>Developer</td>
-                  <td>Aviation</td>
-                </tr>
+
+
               </tbody>
             </table>
           </div>
